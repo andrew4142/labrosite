8bd51ec491804d813c2340fe2734b22b152b5ef7
Andrew work <andrew4142@github.com>
2015-09-03 20:57:11 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+labrosite
+=========
+
+A Symfony project created on August 31, 2015, 1:53 pm.
---
+++ src/Acme/HelloBundle/Controller/ByeController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Acme\HelloBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ByeController extends Controller
+{
+}
