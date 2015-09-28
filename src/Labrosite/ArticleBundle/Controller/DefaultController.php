dfd001ac70a848f056924692d812a2c16831b2cb
Andrew work <andrew4142@github.com>
2015-09-28 20:42:12 +0300

    add show action method for articles

--- src/Labrosite/ArticleBundle/Controller/DefaultController.php
+++ src/Labrosite/ArticleBundle/Controller/DefaultController.php
@@ -6,8 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('LabrositeArticleBundle:Default:index.html.twig', array('name' => $name));
+		$em = $this->getDoctrine()->getManager();
+
+        $articles = $em->getRepository('LabrositeArticleBundle:Article')->findAll();
+		
+		return $this->render('LabrositeArticleBundle:Default:index.html.twig', array('articles' => $articles));
+    }
+	
+    public function showAction($id)
+    {
+		$em = $this->getDoctrine()->getManager();
+
+        $article = $em->getRepository('LabrositeArticleBundle:Article')->findOneBy(array('id'=>$id));
+		
+		return $this->render('LabrositeArticleBundle:Default:show.html.twig', array('article' => $article));
     }
 }
